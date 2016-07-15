7f6e6c6781a9579ee6524d10c0bec7db2d6aaebe
geocklam <geocklam.git@gmail.com>
2016-07-15 16:42:27 -0500

    Added cubic interpolation.

--- cgal_mesh_generation/image_level_surface_to_mesh_converter.h
+++ cgal_mesh_generation/image_level_surface_to_mesh_converter.h
@@ -1,10 +1,8 @@
 #ifndef SKULL_ATLAS_CGAL_MESH_GENERATION_IMAGE_LEVEL_SURFACE_TO_MESH_CONVERTER_H
 #define SKULL_ATLAS_CGAL_MESH_GENERATION_IMAGE_LEVEL_SURFACE_TO_MESH_CONVERTER_H
 
-#include <limits>
 #include <iostream>
 
-#include <CGAL/Exact_predicates_exact_constructions_kernel.h>
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Image_3.h>
 #include <CGAL/Implicit_surface_3.h>
@@ -12,6 +10,8 @@
 #include <CGAL/Polyhedron_3.h>
 #include <CGAL/Surface_mesh_default_triangulation_3.h>
 #include <CGAL/make_surface_mesh.h>
+
+#include "image_level_surface_function.h"
 
 namespace cvc {
 
@@ -34,16 +34,16 @@ class ImageLevelSurfaceToMeshConverter {
 
     typedef typename Surface_mesh_default_triangulation_3_generator<GT>::Type Tr;
 
-    Implicit_surface_3<GT, ImageLevelSurfaceFunction> surface(
-        ImageLevelSurfaceFunction(&image, isovalue),
+    Implicit_surface_3<GT, ImageLevelSurfaceFunction<GT> > surface(
+        ImageLevelSurfaceFunction<GT>(&image, isovalue),
         computeBoundingSphere(image));
 
-    FT facet_angle_lower_bound_in_degrees = 30;
+    FT facet_angle_lower_bound_in_degree = 30;
     // TODO: Choose these parameters carefully.
     FT delaunay_radius_upper_bound = 5;
     FT delaunay_center_to_center_distance_upper_bound = 5;
     Surface_mesh_default_criteria_3<Tr> criteria(
-        facet_angle_lower_bound_in_degrees,
+        facet_angle_lower_bound_in_degree,
         delaunay_radius_upper_bound,
         delaunay_center_to_center_distance_upper_bound);
 
@@ -81,41 +81,6 @@ class ImageLevelSurfaceToMeshConverter {
 
     return Sphere(center, squared_radius);
   }
-
-  // The level set surface of an image.
-  //
-  // Is Model Of: ::CGAL::ImplicitFunction
-  class ImageLevelSurfaceFunction {
-   public:
-
-    typedef typename GT::FT FT;
-
-    typedef typename GT::Point_3 Point;
-
-    // Constructs a level set surface of "image" at "isovalue".
-    ImageLevelSurfaceFunction(const ::CGAL::Image_3* image, FT isovalue)
-      : image_(image),
-        isovalue_(isovalue) {}
-
-    // Returns the value of the image at the "point" subtracted by iso_value.
-    //
-    // If the "point" is outside the image, negative infinity is returned.
-    // Otherwise, trilinear interpolation is used to calculate the value.
-    FT operator()(Point point) const {
-     using namespace ::std;
-
-      FT value_outside = -numeric_limits<FT>::infinity();
-
-      FT image_value = image_->trilinear_interpolation<float, FT>(
-          point.x(), point.y(), point.z(), value_outside);
-
-      return FT(image_value - isovalue_);
-    }
-
-   private:
-    const ::CGAL::Image_3* image_;
-    FT isovalue_;
-  };
 };
 
 } // namespace skull_atlas
