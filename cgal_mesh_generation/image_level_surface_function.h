f9312d7d5abdf0f3289fec0fa35e3903d1ddbc46
geocklam <geocklam.git@gmail.com>
2016-08-23 18:12:11 -0500

    Fixed issues with negative voxel values.
    Minimum voxel value is now used instead of zero for voxels outside
    the image.

--- cgal_mesh_generation/image_level_surface_function.h
+++ cgal_mesh_generation/image_level_surface_function.h
@@ -1,6 +1,8 @@
 #ifndef SKULL_ATLAS_CGAL_MESH_GENERATION_IMAGE_LEVEL_SURFACE_FUNCTION_H
 #define SKULL_ATLAS_CGAL_MESH_GENERATION_IMAGE_LEVEL_SURFACE_FUNCTION_H
 
+#include <algorithm>
+
 #include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
 #include <CGAL/Image_3.h>
 
@@ -23,13 +25,24 @@ class ImageLevelSurfaceFunction {
   ImageLevelSurfaceFunction(const ::CGAL::Image_3* image, double isovalue)
     : image_(image),
       isovalue_(isovalue),
-      interpolation_() {}
+      interpolation_(),
+      min_value_(0.0) {
+
+    for (size_t x0 = 0; x0 != image_->xdim(); ++x0) {
+      for (size_t x1 = 0; x1 != image_->ydim(); ++x1) {
+        for (size_t x2 = 0; x2 != image_->zdim(); ++x2) {
+          min_value_ = ::std::min(
+              static_cast<double>(image_->value(x0, x1, x2)), min_value_);
+        }
+      }
+    }
+  }
 
   // Returns the value of the image at the (x0, x1, x2) subtracted by iso_value.
   double operator()(long x0, long x1, long x2) const {
    using namespace ::std;
 
-    double value = 0.0;
+    double value = min_value_;
 
     if (x0 >= 0 && x0 < image_->xdim()
         && x1 >= 0 && x1 < image_->ydim()
@@ -62,6 +75,7 @@ class ImageLevelSurfaceFunction {
  private:
   const ::CGAL::Image_3* image_;
   double isovalue_;
+  double min_value_;
   CubicInterpolation interpolation_;
 };
 
