561493bd2a3cd9a4f9e4170e13fa715ff826acee
Kevin Song <chipbuster@gmail.com>
2016-12-11 18:54:40 -0600

    Complete testing function

--- scripts/niftireg.r
+++ scripts/niftireg.r
@@ -11,21 +11,26 @@ testImgReg <- function(imgFilename){
   sourceImage <- readNifti(imgFilename)
 
   # Set dummy transformations and print to screen
-  transV <- (runif(3) - 0.5) * 20 # Translations up to 10
-  scaleV <- sqrt(sqrt(runif(3) + 0.5)) # Tends to the [0.8, 1.2] range
+  transV <- (runif(3) - 0.5) * 100 # Translations up to 50
+  scaleV <- 1 +                     
   rotV   <- runif(3) - 0.5        # -0.5 to + 0.5 radians (about 30 degrees max)
   
   cat("Translation vector: ", transV, "\n")
+  cat("Scale vector: ", scaleV, "\n")
   cat("Rotation angles: ", rotV, "\n")
   
   # Build the affine transform and apply it to the image
-  afT = buildAffine(translation=transV, scales=c(1,1,1),angles=rotV, 
+  afT = buildAffine(translation=transV, scales=scaleV,angles=rotV, 
                     source=sourceImage)
   targetImage <- applyTransform(afT,sourceImage)
   
   transform <- niftyreg(sourceImage,targetImage)
   
-  print(decomposeAffine(forward(transform)))
-  print(decomposeAffine(reverse(transform)))
+  decomposed = decomposeAffine(forward(transform))
   
+  deltaTrans = transV - decomposed$translation
+  deltaScale = scaleV - decomposed$scales
+  deltaAngle = rotV - decomposed$angles
+  
+  return(list(deltaTrans,deltaScale,deltaAngle))
 }
