593dca52ab7bb6904f3cb796de4a49cb627d74c1
JimmyGoo <guzc14@mails.tsinghua.edu.cn>
2017-08-23 15:37:45 -0500

    bugs in urls

--- web-viewer/viewer/urls.py
+++ web-viewer/viewer/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
   	url(r'^deform/$', views.skull_deform_frame, name='deformation'),
   	url(r'^similar/$', views.similar_skull, name='similarity'),
   	url(r'^skull_detail/$', views.skull_detail, name='skull_detail'),
-  	url(r'^skulls/$', views.show_skulls, name 'skulls'),
+  	url(r'^skulls/$', views.show_skulls, name='skulls'),
   	url(r'^$', views.index, name='index'),
 ]
